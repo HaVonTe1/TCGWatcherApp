377cd2d3579116f8744dd02883e7a058616db025
Dirk Kutzer <dirk.kutzer@bdr.de>
2024-06-20 09:54:51 +0200

    rework of search list details

--- app/src/main/java/de/dkutzer/tcgwatcher/ui/CommonUi.kt
+++ app/src/main/java/de/dkutzer/tcgwatcher/ui/CommonUi.kt
@@ -1,8 +1,21 @@
 package de.dkutzer.tcgwatcher.ui
 
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -11,12 +24,14 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import coil.imageLoader
 import coil.request.ImageRequest
 import coil.util.DebugLogger
 import de.dkutzer.tcgwatcher.R
+import de.dkutzer.tcgwatcher.cards.boundary.SearchViewCardIconRow
 import de.dkutzer.tcgwatcher.cards.entity.BaseProductModel
 import java.time.OffsetDateTime
 import java.time.format.DateTimeFormatter
@@ -54,11 +69,16 @@ fun RowScope.TableCell(
         text = text,
         Modifier
             .border(1.dp, Color.Transparent)
-            .weight(weight)
+            .weight(weight, true)
+            .fillMaxWidth()
             .padding(1.dp),
         style = style
     )
 }
+
+private const val referrer = "https://www.cardmarket.com/"
+private const val  userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
+
 @Composable
 fun ItemOfInterestCard(
     productModel: BaseProductModel,
@@ -68,7 +88,7 @@ fun ItemOfInterestCard(
 ) {
 
     Card(
-        modifier = modifier.padding(8.dp),
+        modifier = modifier.padding(1.dp),
         elevation = CardDefaults.cardElevation()
     ) {
         Row(
@@ -76,39 +96,38 @@ fun ItemOfInterestCard(
                 .padding(1.dp)
                 .fillMaxWidth()
         ) {
-            val userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
             AsyncImage(
 
                 model = ImageRequest.Builder(LocalContext.current)
                     .data(productModel.imageUrl)
                     .setHeader("User-Agent", userAgent)
-                    .setHeader("Referer", "https://www.cardmarket.com/") //TODO: cloudflare protection is kicking in without the referer
+                    .setHeader("Referer", referrer) //TODO: cloudflare protection is kicking in without the referer
                     .build(),
 
                 contentDescription = productModel.id,
                 modifier = Modifier
-                    .padding(4.dp)
-                    .width(100.dp),
-                contentScale = ContentScale.Fit,
+                    .weight(.3f, true)
+                    .padding(1.dp),
+                contentScale = ContentScale.FillWidth,
                 imageLoader = LocalContext.current.imageLoader.newBuilder().logger(DebugLogger()).build()
             )
             Column(
                 modifier = Modifier
-                    .padding(1.dp)
-                    .fillMaxSize(),
+                    .weight(.7f, false)
+                    .fillMaxWidth()
+                    .padding(1.dp),
                 verticalArrangement = Arrangement.SpaceBetween
             ) {
-
-
                 ItemDetailsTable(
                     localName = productModel.localName,
                     price = productModel.intPrice,
                     showLastUpdated = showLastUpdated,
                     lastUpdated = OffsetDateTime.now(),
-                    modifier = Modifier.padding(1.dp))
+                    modifier = Modifier
+                        .padding(4.dp)
+                        .fillMaxHeight(.9f))
                 Spacer(modifier = Modifier.height(16.dp)) // doesnt work with arrangement. why?
                 iconRowContent()
-
             }
         }
     }
@@ -126,15 +145,17 @@ fun ItemDetailsTable(
     val column1Weight = .3f // 30%
     val column2Weight = .7f // 70%
     // The LazyColumn will be our table. Notice the use of the weights below
-    Column(modifier = modifier)
+    Column(modifier = modifier.padding(1.dp))
     {
-
         Row(Modifier.fillMaxWidth()) {
-            TableCell(text = stringResource(id = (R.string.nameLabel)), weight = column1Weight, MaterialTheme.typography.labelMedium)
+            TableCell(
+                text = stringResource(id = (R.string.nameLabel)),
+                weight = column1Weight,
+                style = MaterialTheme.typography.labelMedium)
             TableCell(
                 text = localName,
                 weight = column2Weight,
-                MaterialTheme.typography.labelLarge
+                style =  MaterialTheme.typography.headlineMedium
             )
         }
 
@@ -147,7 +168,7 @@ fun ItemDetailsTable(
             TableCell(
                 text = price,
                 weight = column2Weight,
-                MaterialTheme.typography.labelLarge
+                MaterialTheme.typography.headlineLarge
             )
         }
         if(showLastUpdated) {
@@ -170,6 +191,21 @@ fun ItemDetailsTable(
 }
 
 
+@Preview
+@Composable
+fun ItemOfInterestCardPreview() {
+    ItemOfInterestCard(
+        productModel = BaseProductModel(
+            id = "bla",
+            imageUrl = "https://product-images.s3.cardmarket.com/51/TEF/760774/760774.jpg",
+            intPrice = "10,00 €",
+            localName = "bbbbb",
+            detailsUrl = "https://product-images.s3.cardmarket.com/51/TEF/760774/760774.jpg"
+        ),
+        showLastUpdated = true,
+        iconRowContent = { SearchViewCardIconRow() }
 
+    )
+}
 
 
